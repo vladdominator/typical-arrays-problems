a4023f107f0f53811b2e1646d7d0188226700ea8
vladdominator <73780858+vladdominator@users.noreply.github.com>
2021-02-08 18:25:29 +0300

    feat: thats all

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+   if(arguments.length === 0 || array.length === 0){
+      return 0;
+   }
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+   if(arguments.length === 0 || array.length === 0){
+      return 0;
+   }
+   return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+   if(arguments.length === 0 || array.length === 0){
+      return 0;
+   }
+  let length = array.length;
+  let simple = array.reduce((acc, item) => acc + item, 0);
+  return simple/length;
 }
